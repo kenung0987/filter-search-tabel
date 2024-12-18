ca7108e9373dbb81867a58b439963a0af6b78fc3
kenung0987 <54462061+kenung0987@users.noreply.github.com>
2024-12-18 14:56:19 +0700

    App.vue

---
+++ .github/workflows/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1>Filter Search Tabel NIK</h1>
+    <input type="text" v-model="searchQuery" placeholder="Cari NIK...">
+    <table border="1">
+      <thead>
+        <tr>
+          <th>NIK</th>
+          <th>Nama</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in filteredItems" :key="index">
+          <td>{{ item.nik }}</td>
+          <td>{{ item.nama }}</td>
+          <td>{{ item.email }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import { dataItems } from './data.js'
+
+export default {
+  data() {
+    return {
+      searchQuery: '',
+      items: dataItems
+    }
+  },
+  computed: {
+    filteredItems() {
+      return this.items.filter(item => {
+        return item.nik.includes(this.searchQuery) ||
+               item.nama.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
+               item.email.toLowerCase().includes(this.searchQuery.toLowerCase())
+      })
+    }
+  }
+}
+</script>
+  
